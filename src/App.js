48f359dee723bcdba0e8ae0b7c6d0a7924b7410d
Aditya Cherukuri <adityacherukuri@Adityas-MacBook-Pro.local>
2020-12-19 12:32:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Nav from "./Nav";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="TRENDING NOW" fetchURL={requests.fetchTrending} />
+      <Row title="TOP RATED" fetchURL={requests.fetchTopRated} />
+      <Row title="ACTION MOVIES" fetchURL={requests.fetchActionMovies} />
+      <Row title="COMEDY MOVIES" fetchURL={requests.fetchComedyMovies} />
+      <Row title="HORROR MOVIES" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE MOVIES" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="DOCUMENTARIES" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
